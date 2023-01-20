53c07227db48f29d28e2543baab5462dad783731
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-01-19 18:26:37 -0700

    geolocation

--- iss.js
+++ iss.js
@@ -8,7 +8,7 @@
  */
 const request = require("request");
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = (callback) => {
   // use request to fetch IP address from JSON API
   let ip = null;
 
@@ -24,9 +24,9 @@ const fetchMyIP = function(callback) {
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      
+
       callback(Error(msg), null);
-      
+
       return;
     }
 
@@ -37,4 +37,39 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  let data = null;
+
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+
+      callback(Error(msg), null);
+
+      return;
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+
+      callback(Error(message), null);
+
+      return;
+    }
+
+    const { latitude, longitude } = parsedBody;
+    data = { latitude, longitude };
+
+    callback(error, data);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
